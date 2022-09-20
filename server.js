e069b639b9d71a10bf1711cf59b7c6dcca1d95d7
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-09-20 14:26:20 -0400

    defines model and mounts middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 const express = require('express');
 const mongoose = require('mongoose');
+const logger = require('morgan');
 // Initialize the express app
 const app = express();
 // Config applications settings
@@ -14,7 +15,21 @@ mongoose.connect(DATABASE_URL);
 mongoose.connection
 .on('connected' , () => console.log('Connected to MongoDB'))
 .on('error' , (error) => console.log('Problem with MongoDB: ' + error.message));
+
+// Set up model
+const peopleSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    title: String
+}, {timestamps: true});
+
+const People = mongoose.model('People', peopleSchema);
+
 // Mount middleware
+// app.use(express.urlencoded({ extended: false })) // intercepts incoming form data and turns it into req.body
+app.use(express.json()); // This middleware intercepts incoming json requests bodies and turns them into req.body
+app.use(logger('dev'));
+
 
 // Mount routes
 app.get('/', (req, res) => {
